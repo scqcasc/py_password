24a8531e844ba6818b0d14d4f7592f6a7443d509
Sean Quaint <seanquaint@zohomail.com>
2024-07-16 19:53:28 -0400

    set default to 8

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--length', '-l', type=int, default=4)
+    parser.add_argument('--length', '-l', type=int, default=8)
     args = parser.parse_args()
 
     p = Password(length=args.length)
